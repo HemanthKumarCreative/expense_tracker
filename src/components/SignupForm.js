fecf10ce229ceb19fe77595809b32dd678b915fd
Hemanth Kumar <avisihks@gmail.com>
2023-09-19 14:25:45 +0530

    Add Signup Form

---
+++ src/components/SignupForm.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
+
+const SignupForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:5000/api/users/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("User created:", data);
+      } else {
+        console.error("Error:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}>
+        <TextField
+          id="name"
+          label="Name"
+          variant="outlined"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <TextField
+          id="email"
+          label="Email"
+          variant="outlined"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <TextField
+          id="password"
+          label="Password"
+          variant="outlined"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <Button variant="contained" type="submit">
+          Sign Up
+        </Button>
+      </Box>
+    </form>
+  );
+};
+
+export default SignupForm;
